6b4e6155d927bb4a20db36b686c9b3806e6678a1
marcooabdo <marcooabdo@gmail.com>
2026-05-15 11:27:41 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import {
-  getComponentChunkLinks,
-  getFontFaceStyles,
-  getFontLinks,
-  getIconLinks,
-  getInitialStyles,
-  getMetaTagsAndIconLinks,
-} from '@porsche-design-system/components-react/partials';
 
 const transformIndexHtmlPlugin = () => {
   return {
     name: 'html-transform',
-    transformIndexHtml(html: string) {
+    async transformIndexHtml(html: string) {
+      const {
+        getComponentChunkLinks,
+        getFontFaceStyles,
+        getFontLinks,
+        getIconLinks,
+        getInitialStyles,
+        getMetaTagsAndIconLinks,
+      } = await import('@porsche-design-system/components-react/partials');
+
       const headPartials = [
         getInitialStyles(),
         getFontFaceStyles(),
